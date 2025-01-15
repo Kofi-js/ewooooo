631798a38f36e3f749c89f14f4304c63833f7de1
kofi-js <vwegbakofi@gmail.com>
2025-01-16 00:37:40 +0100

    Added Skills Section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Section/Hero'
+import Skills from '../Section/Skills'
 
 function Home() {
   return (
     <div>
         <Hero/>
+        <Skills/>
     </div>
   )
 }
